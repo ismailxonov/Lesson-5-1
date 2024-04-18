36f2bf408f9ddeb98c3aeaeab74420f014ae1956
ismailxonov <ismailxonovsiratulloh@gmail.com>
2024-04-18 07:40:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var son = prompt("Iltimos 1 dan 7 gacha bo'lgan bir son kiriting:");
+switch (parseInt(son)) {
+    case 1:
+        alert("Dushanba");
+        break;
+    case 2:
+        alert("Seshanba");
+        break;
+    case 3:
+        alert("Chorshanba");
+        break;
+    case 4:
+        alert("Payshanba");
+        break;
+    case 5:
+        alert("Juma");
+        break;
+    case 6:
+        alert("Shanba");
+        break;
+    case 7:
+        alert("Yakshanba");
+        break;
+    default:
+        alert("Hafta kunlarining soni 1 dan 7 gacha bo'lishi kerak.");
+}
